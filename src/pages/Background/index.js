a561c1305ad5c78fc0a1ff122531fb8777da7ea5
Monokuna-Hugo <70012576+Monokuna-Hugo@users.noreply.github.com>
2025-09-18 15:10:07 +0800

    编写数据处理事件函数

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,1 +1,35 @@
 
+class DataBackGround {
+    constructor() {
+        this.init();
+    }
+
+    init() { }
+
+    messageHandler(request, sender, sendResponse) {
+        const actions = {
+            onDataSave: () => this.onDataSave(request, sendResponse),
+            onDataPageOpen: () => this.onDataPageOpen(),
+        };
+
+        if (Object.prototype.hasOwnProperty.call(actions, request.action)) {
+            actions[request.action]();
+        }
+        return false;
+    }
+
+    onDataSave(request, sendResponse) {
+        chrome.storage.local.set({ studentInfo: request.data }, () => {
+            chrome.runtime.lastError ? sendResponse({ success: false }) : sendResponse({ success: true });
+        })
+        return true;
+    }
+
+    onDataPageOpen() {
+        chrome.runtime.openOptionsPage();
+        return true;
+    }
+
+}
+
+chrome.runtime.onMessage.addListener(new DataBackGround().messageHandler.bind(new DataBackGround()));
