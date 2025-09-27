a828ea56afd50f7c76d71d9430712690172c268b
Monokuna-Hugo <70012576+Monokuna-Hugo@users.noreply.github.com>
2025-09-27 15:39:18 +0800

    feat(Options): 在头部添加实时时间显示
    添加了实时时间显示功能，使用useState和useEffect来更新时间，并格式化显示在页面头部右侧

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Flex, Layout, Typography } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Flex, Layout, Statistic, Typography } from 'antd';
 import { HashRouter as Router } from 'react-router-dom';
 import MySider from '../../containers/MySider/MySider';
 import AppRoutes from '../../containers/Routes/AppRoutes';
 
 const { Header, Footer, Content } = Layout;
 const { Title } = Typography;
+const { Timer } = Statistic;
 
 const headerStyle = {
   paddingInline: '20px',
   background: '#fff',
   color: 'unset',
   marginBottom: '1px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 };
 
 const contentStyle = {
@@ -45,7 +49,32 @@ const headerContentStyle = {
   height: '64px',
 };
 
+const TimerStyle = {
+  fontSize: '14px',
+  fontWeight: 'bold',
+  color: 'black',
+}
+
 const Options = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const Timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(Timer)
+  }, []);
+
+  const formatTime = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+  }
+
   return (
     <Router>
       <Layout style={layoutStyle}>
@@ -54,6 +83,9 @@ const Options = () => {
           <Header style={headerStyle}>
             <div className='header-content' style={headerContentStyle}>
               <Title className='header-title' style={titleStyle}>智慧3.0助手</Title>
+            </div>
+            <div style={TimerStyle}>
+              {formatTime(currentTime)}
             </div>
           </Header>
           <Content style={contentStyle}>
